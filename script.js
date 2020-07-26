2c5ce3e5bad2b0a1957776002ad87c4d275a8027
hureta <40241920+0t3k25@users.noreply.github.com>
2020-07-26 11:22:14 +0900

    add Game Over

--- script.js
+++ script.js
@@ -7,6 +7,31 @@ var dx = 2;
 var dy = -2;
 var ballRadius = 10;
 var ballColor = "red";
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown",keyDownHandler,false);
+document.addEventListener("keyup",keyUpHandler,false);
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x,y,ballRadius,0,Math.PI*2);
@@ -15,25 +40,55 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw(){
 
     const ballColorArray = ["red","green","blue","#0095DD"];
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    drawBall();
     
-    if (y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
+    drawBall();
+    drawPaddle();
+
+    if (y + dy < ballRadius) {
         dy = -dy;
         ballColor = ballColorArray[Math.floor(Math.random()*4)];
-    } else if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
+    }else if(y +dy > canvas.height-ballRadius){
+        //このif文意味不明
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            //なんやこれ
+            dy = -dy ;
+        } else {
+        alert("GAME OVER");
+        document.location.reload();
+        clearInterval(interval);
+        }
+    }
+
+    
+    if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
         dx = -dx;
         ballColor = ballColorArray[Math.floor(Math.random()*4)];
     } 
     
+
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 3; 
+    } else if (leftPressed && paddleX >0) {
+        paddleX -= 3;
+    } 
     x += dx;
     y += dy;
 }
 
-setInterval(draw,0);
+var interval = setInterval(draw,10);
 
 
 
